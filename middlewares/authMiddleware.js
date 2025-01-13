2491419b7554f7e2548a0f8e67fa3d783e35ce50
zjleee <zjlee8@gmail.com>
2025-01-13 03:23:43 +0000

    Add test client

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,7 +4,7 @@ const JWT_SECRET = process.env.JWT_SECRET;
 // Token Authentication Middleware
 function authenticateToken(req, res, next) {
     const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1]; // Extract token from "Bearer <token>"
+    const token = authHeader && authHeader.split(" ")[1];
 
     if (!token) {
         return res.status(401).json({ message: "Unauthorized access" });
